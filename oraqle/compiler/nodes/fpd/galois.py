69a8e54d0f87e34afac55d6ea5e4db8c8b5fbc77
Jelle Vos <github@jelle-vos.nl>
2025-03-07 16:52:34 +0100

    Add required fields

--- oraqle/compiler/nodes/fpd/galois.py
+++ oraqle/compiler/nodes/fpd/galois.py
@@ -1,15 +1,38 @@
 from typing import Type
 
 from galois import FieldArray
+from oraqle.compiler.nodes.fp.abstract import CostParetoFront
+from oraqle.compiler.nodes.fp.fixed import ArithmeticNode
 from oraqle.compiler.nodes.fp.univariate import UnivariateFpNode
 from oraqle.compiler.nodes.fpd.abstract import FpdNode
 
 
 class FieldNorm(UnivariateFpNode[FpdNode]):
+
+    @property
+    def _node_label(self) -> str:
+        return "Norm"
+    
+    @property
+    def _hash_name(self) -> str:
+        return "field_norm"  # TODO: Should we include gf in the hash?
+    
+    @property
+    def _node_shape(self) -> str:
+        return "box"
     
     def __init__(self, node: FpdNode, gf: Type[FieldArray]) -> None:
         # TODO: Implement the arithmetization
         super().__init__(node, gf)
+
+    def _arithmetize_inner(self, strategy: str) -> ArithmeticNode:
+        raise NotImplementedError("TODO")
+    
+    def _arithmetize_depth_aware_inner(self, cost_of_squaring: float) -> CostParetoFront:
+        raise NotImplementedError("TODO")
+    
+    def _operation_inner(self, input: FieldArray) -> FieldArray:
+        return input.field_norm()
 
 
 # TODO: Trace implementation
