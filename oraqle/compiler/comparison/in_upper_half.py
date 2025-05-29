163d1cf8915dabe6493f44c9ab8e22be8cd5f841
Jelle Vos <github@jelle-vos.nl>
2025-05-29 16:50:52 +0200

    Compute coefficients more efficiently for iz

--- oraqle/compiler/comparison/in_upper_half.py
+++ oraqle/compiler/comparison/in_upper_half.py
@@ -223,15 +223,7 @@ class IliashenkoZuccaInUpperHalf(UnivariateNode):
         # TODO: This is copied from above
         # From: Faster homomorphic comparison operations for BGV and BFV, Ilia Iliashenko & Vincent Zucca, 2021
         p = self._gf.characteristic
-        for i in range(p - 1):
-            if i % 2 == 0:
-                # Ignore every even power, we take care of this by squaring the input node.
-                continue
-
-            coefficient = self._gf(0)
-            for a in range(1, p // 2 + 1):
-                coefficient += self._gf(a) ** (p - 1 - i)
-            coefficients.append(coefficient)
+        coefficients = [self._gf(coeff) for coeff in compute_coeffs(p)]
 
         # We do not add the final coefficient, which will be computed later, so we do not do coefficients.append(gf((p + 1) // 2))
 
--- oraqle/experiments/depth_aware_arithmetization/logistic_regression.py
+++ oraqle/experiments/depth_aware_arithmetization/logistic_regression.py
@@ -107,35 +107,37 @@ if __name__ == "__main__":
     dot_product = sum_(*[ConstantMultiplication(inputs[i], gf(int(w) % p)) for i, w in zip(range(X.shape[1]), weights)])
     logit = dot_product + int(intercept)
     
-    # Depth aware
-    prediction = logit < (p // 2)
+    if False:
+        # Depth aware
+        prediction = logit < (p // 2)
 
-    circuit = Circuit(outputs=[prediction])
-    start = time.monotonic()
-    arithmetizations = circuit.arithmetize_depth_aware()
-    print("arith", time.monotonic() - start)
-    d, c, ac = arithmetizations[0]
-    print(d, c)
-    # start = time.monotonic()
-    # ac.eliminate_subexpressions()
-    # print("cse", time.monotonic() - start)
-    # print(ac.multiplicative_depth(), ac.multiplicative_cost(1.0))
+        circuit = Circuit(outputs=[prediction])
+        start = time.monotonic()
+        arithmetizations = circuit.arithmetize_depth_aware()
+        print("arith", time.monotonic() - start)
+        d, c, ac = arithmetizations[0]
+        print(d, c)
+        # start = time.monotonic()
+        # ac.eliminate_subexpressions()
+        # print("cse", time.monotonic() - start)
+        # print(ac.multiplicative_depth(), ac.multiplicative_cost(1.0))
 
-    ac.generate_code("logistic_regression_helib.cpp", measure_time=True, decrypt_outputs=True)
-    ac.generate_code_openfhe("logistic_regression_openfhe.cpp", measure_time=True, decrypt_outputs=True)
+        ac.generate_code("logistic_regression_helib.cpp", measure_time=True, decrypt_outputs=True)
+        ac.generate_code_openfhe("logistic_regression_openfhe.cpp", measure_time=True, decrypt_outputs=True)
 
-    # Previous work
-    prediction = IliashenkoZuccaLessThan(logit, Constant(gf(p // 2)), gf)
+    if True:
+        # Previous work
+        prediction = IliashenkoZuccaLessThan(logit, Constant(gf(p // 2)), gf)
 
-    circuit = Circuit(outputs=[prediction])
-    start = time.monotonic()
-    ac = circuit.arithmetize()
-    print("arith", time.monotonic() - start)
-    print(ac.multiplicative_depth(), ac.multiplicative_cost(1.0))
-    # start = time.monotonic()
-    # ac.eliminate_subexpressions()
-    # print("cse", time.monotonic() - start)
-    # print(ac.multiplicative_depth(), ac.multiplicative_cost(1.0))
+        circuit = Circuit(outputs=[prediction])
+        start = time.monotonic()
+        ac = circuit.arithmetize()
+        print("arith", time.monotonic() - start)
+        print(ac.multiplicative_depth(), ac.multiplicative_cost(1.0))
+        # start = time.monotonic()
+        # ac.eliminate_subexpressions()
+        # print("cse", time.monotonic() - start)
+        # print(ac.multiplicative_depth(), ac.multiplicative_cost(1.0))
 
-    ac.generate_code("logistic_regression_helib_iz.cpp", measure_time=True, decrypt_outputs=True)
-    ac.generate_code_openfhe("logistic_regression_openfhe_iz.cpp", measure_time=True, decrypt_outputs=True)
+        ac.generate_code("logistic_regression_helib_iz.cpp", measure_time=True, decrypt_outputs=True)
+        ac.generate_code_openfhe("logistic_regression_openfhe_iz.cpp", measure_time=True, decrypt_outputs=True)
